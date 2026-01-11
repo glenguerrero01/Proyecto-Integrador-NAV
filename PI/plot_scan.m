25a1b1219ac984cd9853fcafe7a3daac7f5bc991
glenguerrero01 <glenguerrero82@gmail.com>
2026-01-11 09:34:15 -0500

    v2_LidarSLAM y mapa de ocupación
    Se agregó el código que implementa LidarSLAM y crea un mapa de ocupación

--- PI/plot_scan.m
+++ PI/plot_scan.m
@@ -52,6 +52,65 @@ function plot_scan(bagFile)
     ylabel('y [m]');
     title(sprintf('Topic /scan – %d puntos de obstáculo', numel(x)));
 
+  % -- Implementación de LidarSLAM% 
+% Parámetros ajustables
+mapResolution = 15;     % Celdas por metro
+maxLidarRange = 25;     % Dist máx lidar en Metros (Se usa un poco menor según recomendación matlab(Referencias))
+skipStep      = 40;      % Procesar 1 de cada X mensajes (para velocidad)
 
-    
+% Inicializar algoritmo SLAM
+slamAlg = lidarSLAM(mapResolution, maxLidarRange);
+slamAlg.LoopClosureThreshold = 210;  
+slamAlg.LoopClosureSearchRadius = 3; %8
+%slamAlg.MapResolution = mapResolution;
+
+rangoInicial = 20;
+rangoFinal = 100; %45
+
+%% 3. Bucle de Procesamiento
+
+for i=rangoInicial:rangoFinal %Desde 15 a 30
+    s = scanMsgs{i};
+    ranges = double(s.Ranges(:)); 
+    ranges(isnan(ranges)) = Inf;
+
+    N = numel(ranges);
+    % Cálculo de ángulos
+    angles = double(s.AngleMin + (0:N-1)' * s.AngleIncrement);
+    scanObj = lidarScan(ranges, angles);    % objeto para el mapeo
+
+    [isScanAccepted, loopClosureInfo, optimizationInfo] = addScan(slamAlg, scanObj); %scanObj
+    if isScanAccepted
+        fprintf('Added scan %d \n', i);
+    end
+
+end
+
+figure;
+show(slamAlg);
+title({sprintf('LidarSLAM - Map Res: %d, I Range: %d, F Range: %d,   Threshold: %d,  Radius: %d', ...
+    mapResolution, rangoInicial, rangoFinal, slamAlg.LoopClosureThreshold, slamAlg.LoopClosureSearchRadius)});
+
+
+    %% 4. Generación y Visualización del Mapa
+disp('Construyendo mapa de ocupación...');
+
+% Obtener los escaneos y las poses optimizadas tras el SLAM
+[scans, optimizedPoses] = scansAndPoses(slamAlg);
+
+map = buildMap(scans, optimizedPoses, mapResolution, maxLidarRange);
+
+% Visualización Final
+figure();
+show(map);
+hold on
+
+show(slamAlg.PoseGraph, 'IDs', 'off');
+hold off
+
+title('Mapa de Ocupación (Occupancy Grid)');
+xlabel('X [m]');
+ylabel('Y [m]');
+axis equal;
+
 end
